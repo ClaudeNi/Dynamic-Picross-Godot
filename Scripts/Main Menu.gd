60dd0f1e043150f8844d7e1dc88061cb5a458f95
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-06-28 19:22:16 +0300

    added quit game to title screen

--- Scripts/Main Menu.gd
+++ Scripts/Main Menu.gd
@@ -1,5 +1,14 @@
 extends Control
 
+onready var btn1 = $Button
 
 func _on_Button_pressed():
 	get_tree().change_scene("res://nodes/Level Selection Screen.tscn")
+
+
+func _on_Button2_pressed():
+	get_tree().quit()
+
+
+func _on_Button_mouse_entered():
+	pass # Replace with function body.
---
+++ Scripts/Main.gd
@@ -0,0 +1,38 @@
+extends Node2D
+
+onready var img = load(Levels.select_puzzle(Game.selected_puzzle)).get_data()
+
+onready var tilemap = $TileMap
+onready var nums = $nums
+onready var label = $nums/Label
+onready var level_Finished = $LevelFinished
+onready var answer = $LevelFinished/Answer
+
+var SCALE
+var PREVIOUS_CELL = {}
+var PREVIOUS_STATE
+var STOPPER = true
+const PIXEL = 16
+
+func _ready():
+	img.lock()
+	Game.ready_Game(img, tilemap, nums, label)
+
+func _input(event):
+	if (Input.is_action_pressed("left_click") or Input.is_action_pressed("right_click")):
+		SCALE = tilemap.scale.x
+		var x = floor(event.position[0] / (PIXEL * SCALE))
+		var y = floor(event.position[1] / (PIXEL * SCALE))
+		var state = tilemap.get_cell(x,y)
+		if STOPPER:
+			STOPPER = false
+			PREVIOUS_STATE = state
+		if (not PREVIOUS_CELL.get("x") == x or not PREVIOUS_CELL.get("y") == y) and PREVIOUS_STATE == state:
+			Game.decide_Action(x, y, tilemap, level_Finished, answer)
+			PREVIOUS_CELL["x"] = x
+			PREVIOUS_CELL["y"] = y
+	if (Input.is_action_just_released("left_click") or Input.is_action_just_released("right_click")):
+		STOPPER = true
+		PREVIOUS_CELL.clear()
+	#print(Game.vertical_nums)
+	#print(Game.horizontal_nums)
