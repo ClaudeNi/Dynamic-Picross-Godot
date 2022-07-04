fb04251b3f9c21106f06d174281c7606c2d9450d
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-07-04 13:41:16 +0300

    updated level select screen

--- Scripts/Level Selector.gd
+++ Scripts/Level Selector.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 export var PUZZLE_NAME = ""
+export var PUZZLE_NUMBER = 0
 
 onready var texture_link = load(Levels.select_puzzle(PUZZLE_NAME))
 
@@ -12,6 +13,8 @@ func _ready():
 	if Save.game_data["beaten_levels"].has(PUZZLE_NAME):
 		sprite.texture = texture_link
 		label.text = PUZZLE_NAME.capitalize()
+	else:
+		label.text += str(PUZZLE_NUMBER)
 		
 
 func _on_Button_pressed():
