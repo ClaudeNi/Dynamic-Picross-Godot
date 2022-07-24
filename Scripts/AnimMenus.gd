94d79385fa22bc8a282d75c9b40846cb4554b872
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-07-24 23:55:54 +0300

    Delete AnimMenus.gd

--- Scripts/AnimMenus.gd
+++
@@ -1,9 +0,0 @@
-extends Control
-
-onready var anim = $AnimationPlayer
-
-func level_change_scene():
-	get_tree().change_scene("res://nodes/Level.tscn")
-
-func select_level():
-	anim.play("Select Level")
