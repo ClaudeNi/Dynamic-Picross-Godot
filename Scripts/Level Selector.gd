b389ac9302681229e07f3934ae8b340c798ee5b4
Claude Ni <49127719+ClaudeNi@users.noreply.github.com>
2022-10-06 21:48:39 +0300

    updated level puzzle image scaling

--- Scripts/Level Selector.gd
+++ Scripts/Level Selector.gd
@@ -5,11 +5,20 @@ onready var sprite = $sprite
 onready var label = $Label
 
 onready var texture_link = load(Levels.select_puzzle(btn.PUZZLE_NAME))
+onready var scales = {
+	"10": 15,
+	"15": 10,
+	"20": 8,
+}
 
 func _ready():
 	add_to_group("Level")
 	if Save.game_data["beaten_levels"].has(btn.PUZZLE_NAME):
 		sprite.texture = texture_link
+		texture_link.get_data().lock()
+		var WIDTH = str(texture_link.get_width())
+		sprite.scale.x = scales[WIDTH]
+		sprite.scale.y = scales[WIDTH]
 		label.text = btn.PUZZLE_NAME.capitalize()
 	else:
 		label.text += str(btn.LEVEL_NUMBER)
